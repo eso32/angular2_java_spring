3d78a2f0cd6f91ea193ef1331a65f84b33956f2f
Tomasz Szłapa <tomek.szlapa@gmail.com>
2018-01-16 22:14:14 +0100

    Fix tomcat

--- src/main/java/io/javabrains/CourseApiApp.java
+++ src/main/java/io/javabrains/CourseApiApp.java
@@ -2,17 +2,10 @@ package io.javabrains;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CourseApiApp extends SpringBootServletInitializer{
+public class CourseApiApp {
     public static void main(String[] args) {
         SpringApplication.run(CourseApiApp.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(CourseApiApp.class);
     }
 }
