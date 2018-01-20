3244d62972fc15bd2a4ca8846141e3b7723f1c91
Tomasz Szłapa <tomek.szlapa@gmail.com>
2018-01-20 16:36:34 +0100

    Edit baseuri

--- src/main/webapp/app/app.js
+++ src/main/webapp/app/app.js
@@ -4,9 +4,11 @@ angular.module("myApp").controller("mainCtrl", function($scope, $http){
     $scope.idk;
     $scope.name;
     $scope.desc;
+    var baseURI = 'https://infinite-springs-25353.herokuapp.com/';
+    var baseUR = 'http://localhost:8090/topics';
 
     $scope.sendGet = function(){
-        $http.get('http://localhost:8090/topics').
+        $http.get(baseURI).
                 then(function(response) {
                     $scope.topics = response.data;
                 });
@@ -22,7 +24,7 @@ angular.module("myApp").controller("mainCtrl", function($scope, $http){
 
         console.log('Func invoked');
         console.log($scope.toBeSend);
-        $http.post('http://localhost:8090/topics', $scope.toBeSend).
+        $http.post(baseURI, $scope.toBeSend).
                 then(function(response) {
                     console.log(response);
                     $scope.post = response;
